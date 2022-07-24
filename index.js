719f75f95221c3b07b0d23b0dc60989f87ca13ee
Marshall Paradox <109531746+MarshallParadox@users.noreply.github.com>
2022-07-24 11:52:18 +0530

    Queen Aurora ❤- Public Version 📢

---
+++ index.js
@@ -0,0 +1,13 @@
+const client = require('./lib/client')
+const { DATABASE, VERSION } = require('./config')
+const start = async () => {
+	try {
+		console.log(`QueenAurora ${VERSION}`)
+		await DATABASE.sync()
+		console.log('DB syncing')
+		await client.connect()
+	} catch (error) {
+		console.error(error)
+	}
+}
+start()
